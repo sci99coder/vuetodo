d496ee91161e4a84daf25fdcb53458b39426ab69
coder <272475382@qq.com>
2018-04-24 23:24:31 +0800

    布局整体页面

--- src/app.vue
+++ src/app.vue
@@ -2,17 +2,20 @@
     <div id="app">
         <div id="cover"></div>
         <Header></Header>
+        <todo></todo>
         <Footer></Footer>
     </div>
 </template>
 <script>
 import Header from './todo/header.vue'
 import Footer from './todo/footer.jsx'
+import Todo from './todo/todo.vue'
 
 export default {
     components:{
         Header,
-        Footer
+        Footer,
+        Todo
     }
 }
 </script>
--- src/todo/footer.jsx
+++ src/todo/footer.jsx
@@ -1,10 +1,15 @@
 import "../assets/styles/footer.styl"
 
 export default{
+    data(){
+        return {
+            author:'jack'
+        }
+    },
     render(){
         return(
             <div id="footer">
-                <span>write to Coder</span>
+                <span>write to {this.author}</span>
             </div>
         )
     }
